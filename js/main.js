374897371ec4b9bf4bed53950f21ddba3e3f442e
faulki <nydefx@gmail.com>
2025-03-19 23:39:22 +0100

    sds

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const video = document.getElementById("video");
+
+video.addEventListener("click", () => {
+    if (video.paused) {
+        video.play();
+        video.classList.remove("video-paused"); // Supprime l'effet de dézoom
+        video.classList.add("zoom-effect");
+        document.body.classList.add("darken-bg");
+    } else {
+        video.pause();
+        document.body.classList.remove("darken-bg"); // Retire l'effet sombre
+        video.classList.remove("zoom-effect"); // Retire l'effet de zoom
+        video.classList.add("video-paused"); // Ajoute l'effet de dézoom en transition
+    }
+});
